9d1ffb9ef752d547390c57ffa0f326eeca8167d5
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-27 11:51:15 -0500

    Initial Commit

---
+++ MyDaily/MyDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDailyApp.swift
+//  MyDaily
+//
+//  Created by Joseph DeWeese on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
